afa4f05f6b13f52e442eda6371d5e9cdf439765c
rbaymanova <147691637+rbaymanova@users.noreply.github.com>
2023-11-14 22:35:51 +0500

    Update problem_1.cpp

--- problems/problem_1.cpp
+++ problems/problem_1.cpp
@@ -8,7 +8,7 @@ float problemSolution1(float consumed_water) {
     else if (consumed_water > 30 and consumed_water <= 50) { 
         cost = fixed + ((consumed_water - 30) * 0.12) + (30 * 0.4); 
     } 
-    else if (consumed_water > 50 and consumed_water <= 60) { // Fix: Changed "30" to "50" in the condition 
+    else if (consumed_water > 50 and consumed_water <= 60) {
         cost = fixed + ((consumed_water - 50) * 1.4) + (30 * 0.4) + (20 * 0.12);
     } 
     else if (consumed_water > 60) { 
