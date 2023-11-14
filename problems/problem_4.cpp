94d69ca6aa215b9966db5b82ac6f1e0948089e6e
rbaymanova <147691637+rbaymanova@users.noreply.github.com>
2023-11-14 22:33:38 +0500

    Update problem_4.cpp

--- problems/problem_4.cpp
+++ problems/problem_4.cpp
@@ -1,9 +1,19 @@
-#include <string>
-#include <sstream>
-
-std::string problemSolution4(const std::string &macAddress) {
-    // write your code here
-
-    // make use of control flow statements
-    // return result;
+#include <string> 
+#include <sstream> 
+ 
+std::string problemSolution4(const std::string &macAddress) { 
+    std::istringstream iss(macAddress); 
+    std::string octet; 
+    getline(iss,octet,':'); 
+    int firstOctet; 
+    std::stringstream(octet) >> std::hex >> firstOctet; 
+    if (firstOctet % 2 == 0) { 
+        return "Unicast"; 
+    } 
+    else if (firstOctet %2 != 0 and firstOctet != 255){ 
+        return "Multicast"; 
+    } 
+    else if (firstOctet == 255) { 
+        return "Broadcast"; 
+    } 
 }
