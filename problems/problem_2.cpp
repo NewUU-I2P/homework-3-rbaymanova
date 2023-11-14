3ec7e597b8219e35e3bc4443b61b9c0b3d3cf4e4
rbaymanova <147691637+rbaymanova@users.noreply.github.com>
2023-11-14 22:20:17 +0500

    Update problem_2.cpp

--- problems/problem_2.cpp
+++ problems/problem_2.cpp
@@ -1,6 +1,11 @@
-float problemSolution2(float i, float j, float k) {
-    float result;
-   
-    
-    return result;
+float problemSolution2(float i, float j, float k) { 
+    float result; 
+    result = i; 
+    if (j > result) { 
+        result = j; 
+    } 
+    if (k > result) { 
+        result = k; 
+    } 
+    return result; 
 }
