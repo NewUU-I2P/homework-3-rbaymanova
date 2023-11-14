c74b610bb14e3e47d4074fd021f7c238a62a8f01
rbaymanova <147691637+rbaymanova@users.noreply.github.com>
2023-11-14 22:35:38 +0500

    Update problem_1.cpp

--- problems/problem_1.cpp
+++ problems/problem_1.cpp
@@ -1,20 +1,18 @@
-void problemSolution1(float consumed_water) {
-    float cost;
+float problemSolution1(float consumed_water) { 
+    float cost,fixed; 
+    fixed = 13; 
     
-    cout << "Enter the water consumption in cubic metres: ";
-    cin >> consumed_water;
-
-    if (consumed_water <= 30) {
-        cost = 13 + consumed_water * 0.4;
-    } else if (consumed_water <= 50) {
-        cost = 13 + 30 * 0.4 + (consumed_water - 30) * 0.12;
-    } else if (consumed_water <= 60) {
-        cost = 13 + 30 * 0.4 + 20 * 0.12 + (consumed_water - 50) * 1.4;
-    } else {
-        cost = 13 + 30 * 0.4 + 20 * 0.12 + 10 * 1.4 + (consumed_water - 60) * 1.5;
-    }
-
-    cout << "The total cost is: " << cost << endl;
-
-    return cost;
+    if (consumed_water <= 30) { 
+        cost = fixed + (consumed_water * 0.4); 
+    } 
+    else if (consumed_water > 30 and consumed_water <= 50) { 
+        cost = fixed + ((consumed_water - 30) * 0.12) + (30 * 0.4); 
+    } 
+    else if (consumed_water > 50 and consumed_water <= 60) { // Fix: Changed "30" to "50" in the condition 
+        cost = fixed + ((consumed_water - 50) * 1.4) + (30 * 0.4) + (20 * 0.12);
+    } 
+    else if (consumed_water > 60) { 
+        cost = fixed + ((consumed_water - 60) * 1.5) + (30 * 0.4) + (20 * 0.12) + (10 * 1.4);
+    } 
+    return cost; 
 }
